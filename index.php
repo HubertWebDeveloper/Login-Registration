4e9eca6a4b7266e8485566352ade5e97cb957de5
HubertWebDeveloper <145841547+HubertWebDeveloper@users.noreply.github.com>
2023-10-22 10:31:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="">
+                <h1>Welcome Back</h1>
+                <div class="social-container">
+                    <a href=""><i class="fa fa-facebook-square"></i></a>
+                    <a href=""><i class="fas fa-google"></i></a>
+                    <a href=""><i class="fa fa-linkedin"></i></a>
+                </div>
+                <span>Or use Your Email for Registration</span>
+                <input type="email"placeholder="Email">
+                <input type="password"placeholder="Password">
+                <button>Sign In</button>
+            </form>
+            <div class="content">
+                <h2>Welcome Back!</h2>
+                <p>Enter your personal detailes and Start<br>journary with Us.</p>
+                <a href="register.php" id="signup">Sign Up</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <div class="content">
+                <h2>Welcome Back!</h2>
+                <p>To keep connected with us please login<br>with your personal info.</p>
+                <a href="index.php" id="signin">Sign In</a>
+            </div>
+            <form action="">
+                <h1>Create Account</h1>
+                <div class="social-container">
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-google-plus-g"></i></a>
+                    <a href=""><i class="fab fa-linkedin-in"></i></a>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                </div>
+                <span>Or use Your Email for Registration</span>
+                <input type="text"placeholder="Name">
+                <input type="email"placeholder="Email">
+                <input type="password"placeholder="Password">
+                <button>Sign Up</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
